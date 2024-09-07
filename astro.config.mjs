aaf54d19b4d50ce2a7c9fbcaadf7cdf5b5992f53
ch920026 <35516824+ch920026@users.noreply.github.com>
2024-09-08 01:22:18 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ch920026.github.io",
-  base: "testWeb",
+  base: "website",
 });
